b7d5c38daf4d5f13719f775559629c25422bf0ad
VicDario <26585356+VicDario@users.noreply.github.com>
2024-11-04 17:45:51 -0300

    tets: add tests for stock mapper

---
+++ src/mappers/stock-price-query.mapper.test.ts
@@ -0,0 +1,22 @@
+import { assertEquals, assertInstanceOf } from "jsr:@std/assert";
+import { faker } from "@faker-js/faker";
+import StockPriceQueryMapper from "./stock-price-query.mapper.ts";
+import HttpQueryModel from "../models/http.model.ts";
+import StockPriceModel from "../models/stock-price.model.ts";
+import type { IMapper } from "./IMapper.ts";
+
+Deno.test("StockPriceQueryMapper", async (t) => {
+    await t.step("Map a valid HttpQueryModel instance", () => {
+        const realAssetId = faker.number.int();
+        const date = faker.date.anytime().toLocaleString();
+        const model = new StockPriceModel({ realAssetId, date });
+        const mapper: IMapper<StockPriceModel, HttpQueryModel> =
+            new StockPriceQueryMapper();
+
+        const query = mapper.map(model);
+
+        assertInstanceOf(query, HttpQueryModel);
+        assertEquals(query.path, `/${model.realAssetId}/days`);
+        assertEquals(query.queryParams.get("date"), date);
+    });
+});
